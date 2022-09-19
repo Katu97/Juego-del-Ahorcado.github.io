54e7a303d3befb2c8eac8648a2324673e1110e7d
Katu97 <skatuchin.97.sk@gmail.com>
2022-09-19 14:08:19 -0300

    agrego archivo js

--- javaScript/juego.js
+++ javaScript/juego.js
@@ -1,15 +1,28 @@
 let palabras = ['CALEIDOSCOPIO', 'PROGRAMACION', 'IDIOSINCRASIA', 'PIGMENTO', 'PORCELANA', 'HIPOPOTAMO'];
 let tablero = document.getElementById('horca').getContext('2d');
 let letras = [];
-let palabracorrecta = "";
+let palabraCorrecta = "";
 let errores = 9;
+let palabra = escojerPalabraSecreta();
 
 function escojerPalabraSecreta(){
     let palabra = palabras[Math.floor(Math.random()*palabras.length)];
-    let palabraSecreta = palabra;
-    return palabraSecreta;
+    console.log(palabra);
+    return palabra;
 }
 
 function dibujarLineas(){
-    
-}
+    tablero.lineWidth = 4;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round"; //Redondeo en las lineas
+    tablero.strokeStyle = "#0A3871";
+    tablero.beginPath();
+
+    var ancho = 500/palabra.length;
+    for(let i = 0; i <palabra.length; i++){
+        tablero.moveTo(400+(ancho*i), 300);
+        tablero.lineTo(450+(ancho*i), 300);
+    }
+    tablero.stroke();
+    tablero.closePath();
+}dibujarLineas();
